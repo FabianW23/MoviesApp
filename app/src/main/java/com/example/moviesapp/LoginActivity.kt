efe043de4cfbed93de9eb4e71a53a9141ffc5f1b
FABIAN MORALES DAVID <fabianmorales236962@correo.itm.edu.co>
2022-05-22 22:56:55 -0500

    splash, login and register design and menu navigation

---
+++ app/src/main/java/com/example/moviesapp/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.moviesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviesapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
---
+++ app/src/main/java/com/example/moviesapp/menu/MenuFragment.kt
@@ -0,0 +1,30 @@
+package com.example.moviesapp.menu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.moviesapp.R
+import com.example.moviesapp.databinding.FragmentMenuBinding
+
+
+class MenuFragment : Fragment() {
+
+    private lateinit var binding: FragmentMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentMenuBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
